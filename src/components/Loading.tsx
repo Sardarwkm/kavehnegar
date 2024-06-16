6897b5a375e71f2b3db660c115314943dcdff1b9
sardarwkm <sardarwkm@gmail.com>
2024-06-17 00:41:05 +0330

    refetching-data-after-successful-post

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-dvh'>
+      <span className='text-4xl animate-pulse font-medium text-lime-700'>
+        Loading...
+      </span>
+    </div>
+  );
+};
+
+export default Loading;
